c0dcd41bb86f957bba82ad5b7db27987e2298ca1
vigilija <vigilija.simkeviciute@gmail.com>
2024-01-21 18:34:53 +0100

    Added Dto properties and Auto Mapper.

--- DishesAPI/Program.cs
+++ DishesAPI/Program.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using DishesAPI.DbContexts;
+using DishesAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DishesDbContext>(o => o.UseSqlite(
     builder.Configuration["ConnectionStrings:DishesDBConnectionString"]));
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -14,26 +17,26 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/dishes", async (DishesDbContext dishesDbContext) =>
+app.MapGet("/dishes", async (DishesDbContext dishesDbContext, IMapper mapper) =>
 {
-    return await dishesDbContext.Dishes.ToListAsync();
+    return mapper.Map<IEnumerable<DishDto>> (await dishesDbContext.Dishes.ToListAsync());
 });
 
-app.MapGet("/dishes/{dishId:guid}", async (DishesDbContext dishesDbContext, Guid dishId) =>
-{
-    return await dishesDbContext.Dishes.FirstOrDefaultAsync(d => d.Id == dishId);
+app.MapGet("/dishes/{dishId:guid}", async (DishesDbContext dishesDbContext, IMapper mapper, Guid dishId) =>
+{   
+    return mapper.Map<DishDto>( await dishesDbContext.Dishes.FirstOrDefaultAsync(d => d.Id == dishId));
 });
 
-app.MapGet("/dishes/{dishName}", async (DishesDbContext dishesDbContext, string dishName) =>
+app.MapGet("/dishes/{dishName}", async (DishesDbContext dishesDbContext, IMapper mapper, string dishName) =>
 {
-    return await dishesDbContext.Dishes.FirstOrDefaultAsync(d => d.Name == dishName);
+    return mapper.Map<DishDto>(await dishesDbContext.Dishes.FirstOrDefaultAsync(d => d.Name == dishName));
 });
 
-app.MapGet("/disches/{dishId}", async (DishesDbContext dischDbContext, Guid dishId) =>
+app.MapGet("/disches/{dishId}/ingrediets", async (DishesDbContext dischDbContext, IMapper mapper, Guid dishId) =>
 {
-    return (await dischDbContext.Dishes
+    return mapper.Map<IEnumerable<IngredientDto>>((await dischDbContext.Dishes
     .Include(d => d.Ingredients)
-    .FirstOrDefaultAsync(d => d.Id == dishId))?.Ingredients;
+    .FirstOrDefaultAsync(d => d.Id == dishId))?.Ingredients);
 });
 
 //migrates and recreates DB on each run
